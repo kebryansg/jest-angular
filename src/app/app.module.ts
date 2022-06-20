131dd50615ad3bf7365c4437b9c8ac104c80fb76
Kevin Suarez Guzman <kebryansg@gmail.com>
2022-06-20 10:38:30 -0500

    Implement DS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
